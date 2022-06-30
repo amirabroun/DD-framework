7da00d607c63b4cef074e7d6be65d820b7345060
Amir Abroun <abroon234@gmail.com>
2022-06-30 13:17:19 +0430

    fix some issue

--- resources/Views/auth/login.php
+++ resources/Views/auth/login.php
@@ -2,24 +2,32 @@
 <html lang="en">
 
 <head>
-  <meta charset="UTF-8">
-  <title>Login Page with Background Image Example</title>
-  <link rel="stylesheet" href="<? echo assets('css/login/style.css') ?>">
+    <meta charset="UTF-8">
+    <title>Login</title>
+    <link rel="stylesheet" href="<? echo assets('css/login/style.css') ?>">
 </head>
 
 <body>
-  <div id="bg"></div>
-  <form action="/login/secret/e10adc3949ba59abbe56e057f20f883e" method="POST">
-    <div class="form-field">
-      <input name="username" placeholder="Email / Username" required />
-    </div>
-    <div class="form-field">
-      <input name="password" type="password" placeholder="Password" required />
-    </div>
-    <div class="form-field">
-      <button class="btn" type="submit">Log in</button>
-    </div>
-  </form>
+    <div id="bg"></div>
+    <form action="<? echo '/login/secret/' . md5(secretKey('secret_login')) ?>" method="POST">
+        <div class="form-field">
+            <input name="username" placeholder="Email / Username" required />
+        </div>
+        <div class="form-field">
+            <input name="password" type="password" placeholder="Password" required />
+        </div>
+        <? if (isset($error)) { ?>
+            <div class="form-field">
+                <? echo $error ?>
+            </div>
+        <?
+        } ?>
+        <div class="form-field">
+            <button class="btn" type="submit">Log in</button>
+        </div>
+    </form>
 </body>
+
+<script src="<? echo jsFiles('auth/login.js') ?>"></script>
 
 </html>
