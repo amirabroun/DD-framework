6decebe45eb0d81f3c75b47a2b7a8579b4b7328a
Amir Abroun <abroon234@gmail.com>
2022-06-30 13:19:08 +0430

    chore: delete variable not used

--- app/Helpers/ApiResponse.php
+++ app/Helpers/ApiResponse.php
@@ -4,24 +4,21 @@ namespace App\Helpers;
 
 class ApiResponse
 {
-    public int $status;
-    public string $title;
-    public string|array $message;
-    public array $data;
-    public string $pagination;
-
-    private const RESPONE = [
-        200 => 'success',
-        404 => 'error',
-    ];
 
     /**
-     * @param $pagination
+     * @var int $status
      */
-    public function setPagination($pagination)
-    {
-        $this->pagination = $pagination;
-    }
+    public int $status;
+
+    /**
+     * @var string $title
+     */
+    public string $title = '';
+
+    /**
+     * @var string $message
+     */
+    public string $message = '';
 
     /**
      * @param mixed $status
@@ -29,6 +26,7 @@ class ApiResponse
     public function setStatus($status)
     {
         $this->status = $status;
+
         return $this;
     }
 
@@ -38,75 +36,37 @@ class ApiResponse
     public function setTitle($title)
     {
         $this->title = $title;
+
         return $this;
     }
 
     /**
      * @param mixed $message
      */
-    public function setMessage($message)
+    public function setMessage(array $message)
     {
-        $this->message = $message;
+        $this->message = $this->prepareArrayToString($message);
+
         return $this;
-    }
-
-    private function getTypeResponse(int $status)
-    {
-        return $this::RESPONE[$status];
-    }
-
-    /**
-     * @param $object
-     * @param $description
-     * @param array|null $parametersArray
-     */
-    public function setShortDescription($object, $description)
-    {
-        $explodedMessage = explode(":", $description);
-        $message = $explodedMessage[0];
-        if (isset($explodedMessage[1])) {
-            $message = $explodedMessage[1];
-        }
-        $this->setMessage($message);
-
-        $index = get_class($object);
-        $this->message[$index] = $description;
-    }
-
-    public function sweetAlert()
-    {
-        responseJson([
-            'status' => $this->status,
-            'confirmButtonText' => $this->confirmButtonText ?? 'متوجه شدم!',
-            'message' => [
-                'title' => $this->title,
-                'text' =>  $this->prepareErrorFormForSweetAlert($this->message),
-                'type' => $this->getTypeResponse($this->status),
-            ]
-        ]);
     }
 
     /**
      * @param array $errors
      *
-     * @return string $rules // this is string ready for sweet alert
+     * @return string 
      */
-    public function prepareErrorFormForSweetAlert(array|string $errors)
+    public function prepareArrayToString(array $errors)
     {
-        if (is_string($errors))
-            return $errors;
-
-        $ruleAttributes = attributesTranslate('rule');
+        $ruleAttributes = ruleAttributes();
         $rules = '';
 
         foreach ($errors as $keyI => $i) {
             $cleanError = $errors[$keyI];
 
-            foreach ($cleanError as $input => $error) {
+            foreach ($cleanError as $error) {
                 foreach ($ruleAttributes as $keyRuleAttributes => $rule) {
-
                     if ($keyRuleAttributes == $error) {
-                        $rules .= translate($input) . "\t" . ': ' . ' ' . $rule;
+                        $rules .= $rule;
                     }
                 }
             }
