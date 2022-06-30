8959352db907cc1c712528d773ce8b18911df6e9
Amir Abroun <abroon234@gmail.com>
2022-06-30 13:23:48 +0430

    chore: some documents

--- app/Helpers/Requests.php
+++ app/Helpers/Requests.php
@@ -4,22 +4,59 @@ namespace App\Helpers;
 
 class Requests
 {
-    public function __construct(private $action = null)
+
+    /**
+     * @var array $with
+     */
+    protected $with = [];
+
+    /**
+     * @var string $action
+     */
+    private string $action;
+
+    /**
+     * @param string $action
+     * @return void
+     */
+    public function __construct($action = null)
     {
+        $this->action = $action;
     }
 
+    /**
+     * Action to route
+     * 
+     * @param string $route
+     * @param array $with
+     * @return void
+     */
     public function route($route, array $with = [])
     {
-        ($this->action)($route, $with);
+        "$this->action"($route, $with);
     }
 
+    /**
+     * Action to resource
+     * 
+     * @param string $path
+     * @param array $with
+     * @return void
+     */
     public function resource($path, array $with = [])
     {
-        ($this->action)('/resources' . '/' . preparePath($path), $with);
+        "$this->action"('/resources' . '/' . preparePath($path), $with);
     }
 
+    /**
+     * Action to view
+     * 
+     * @param string $path
+     * @param array $with
+     * @return void
+     */
     public function view($path, array $with = [])
     {
-        ($this->action)('/resources/Views/' . preparePath($path), $with);
+        "$this->action"('/resources/Views/' . preparePath($path), $with);
     }
 }
