5e6a182d16bf3ff7325cc70c0973bc79dfd91f9a
Amir Abroun <abroon234@gmail.com>
2022-07-02 17:01:28 +0430

    feat: add session data to header

--- resources/Views/index.php
+++ resources/Views/index.php
@@ -14,13 +14,21 @@
 <body>
     <!-- navbar start -->
     <nav class="navbar navbar-expand-lg navbar-light bg-light shadow-sm">
-        <a class="navbar-brand" href="#">Blog</a>
+        <span class="navbar-brand">
+            <? if (isset($_SESSION["_admin_log_"])) { ?>
+                Welcome <? echo $_SESSION["_admin_log_"]->first_name . ' ' . $_SESSION["_admin_log_"]->last_name ?> !
+            <?
+            } ?>
+        </span>
         <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
         </button>
         <div class="collapse navbar-collapse" id="navbarSupportedContent">
             <ul class="navbar-nav mr-auto">
-                <li class="nav-item active">
+                <li class="nav-item">
+                    <span class="sr-only">(current)</span>
+                </li>
+                <li class="nav-item">
                     <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
                 </li>
                 <li class="nav-item">
