1902c3f356d91e218ae7b2e2eb9a1041309e3799
Amir Abroun <abroon234@gmail.com>
2022-07-10 18:20:18 +0430

    chore: add new examle for model and query builder

--- routes/web.php
+++ routes/web.php
@@ -1,25 +1,51 @@
 <?php
 
 use App\Models\{Admin, Category, User};
+use App\QueryBuilder\Builder;
 use App\Router\Route;
 
 // test
 Route::get('/test', function () {
+    $Jon = Builder::table('users')
+        ->select(['id', 'first_name'])
+        ->where('id', '<', 12)
+        ->andWhere('first_name', '=', 'Jon')
+        ->orWhere('last_name', '=', 'kelly')
+        ->take(1)->get();
+
+    $newUserWithBuilder = Builder::table('users')->new([
+        'first_name' => 'simin',
+        'last_name' => 'jalili',
+        'mobile' => '09884401',
+    ])->save();
+
+
     $newUser = User::create([
-        'first_name' => 'amir',
-        'last_name' => 'abroun',
-        'mobile' => '09398720306',
+        'first_name' => 'atena',
+        'last_name' => 'aghdasi',
+        'mobile' => '0915789099',
     ]);
-    $newUser->gender = 'male';
+
+    $newUser->gender = 'female';
     $newUser->save();
 
-    $categoreis = Category::query()->where('title', '=', 'admin')->andWhere('id', '=', '3')->get();
-    $user = User::find(81);
+    $category = Category::query()->where('title', '=', 'کالای دیجیتال')->andWhere('id', '=', '3')->get();
+    $user = User::find(2);
     $admin = Admin::query()->where('username', '=', 'admin')->first();
     $users = User::all(['id', 'last_name', 'mobile']);
     $adminLogin = Admin::login('admin', 12345678);
 
-    dd($newUser, $adminLogin, $users, $admin, $user, $categoreis);
+    dd(
+        User::all(),
+        $Jon,
+        $newUser,
+        $newUserWithBuilder,
+        $adminLogin,
+        $users,
+        $admin,
+        $user,
+        $category
+    );
 });
 
 // index
